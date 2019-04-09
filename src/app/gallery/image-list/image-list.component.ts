2a1f9e66ada356b13ab2cad6716c95d4e7929dc2
jhonalejoo <jhonalejoo@gmail.com>
2019-04-08 22:19:24 -0500

    Restaurantapp

---
+++ src/app/gallery/image-list/image-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit,Input } from '@angular/core';
+import {Image} from '../../models/restaurant'
+
+@Component({
+  selector: 'app-image-list',
+  templateUrl: './image-list.component.html',
+  styles: []
+})
+export class ImageListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/models/restaurant.ts
@@ -0,0 +1,3 @@
+export class Restaurant {
+constructor(public id:string, public name:string, public description:string, public url_image:string){}
+}
